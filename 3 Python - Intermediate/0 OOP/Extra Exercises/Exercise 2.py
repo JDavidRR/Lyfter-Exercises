9bb302805e7fb50a7065c9fd4740b4c5c65e2f75
JDavidRR <jdavid.rojasr@icloud.com>
2026-07-31 22:15:36 -0600

    Adding OOP Extra Exercise 1

---
+++ 3 Python - Intermediate/0 OOP/Extra Exercises/Exercise 2.py
@@ -0,0 +1,11 @@
+"""2- Cree una clase base Animal y dos clases hijas Dog y Cat:
+Animal debe tener nombre y método speak() que retorne "Hace un sonido"
+Dog debe sobrescribir speak() para decir "Guau"
+Cat debe sobrescribir speak() para decir "Miau"
+Ejemplo:
+Entrada:
+dog = Dog("Firulais")
+
+Salida:
+print(dog.speak())  # Guau"""
+
